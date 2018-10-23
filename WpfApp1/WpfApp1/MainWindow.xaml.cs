3ce5b5bee625c34407167d57c60c6d383bfaaf0a
sadboi <sadboi@outlook.cz>
2018-10-23 08:03:15 +0200

    init

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -26,51 +26,80 @@ namespace WpfApp1
             generateExample();
         }
         Random rnd = new Random();
+        int points = 1;
+        int sum;
+
         private void generateExample()
         {
-            int num1 = rnd.Next(0, 100);
-            int num2 = rnd.Next(0, 100);
-            int sum = num1 + num2;
-            string numb1 = num1.ToString();
-            string numb2 = num2.ToString();
-            Question.Text = numb1 + " + " + numb2 + "=";
-            Button1.Content = sum;
-            int rndNumOper = rnd.Next(0, 4);
-            int rndOper = rnd.Next(0, 4);
-            int rndsumres = 0;
-            switch (rndOper)
+            if (points <= 10)
             {
-                case 1:
-                    rndsumres = num1 + num2;
-                    break;
-                case 2:
-                    rndsumres = num1 - num2;
-                    break;
-                case 3:
-                    rndsumres = num1 * num2;
-                    break;
-                case 4:
-                    rndsumres = num1 / num2;
-                    break;
-            }
-            if(sum == rndsumres)
-            {
-
+                int num1 = 0;
+                int num2 = 0;
+                if (points < 5)
+                {
+                    num1 = rnd.Next(0, 100);
+                    num2 = rnd.Next(0, 100);
+                }
+                else if (points >= 5)
+                {
+                    num1 = rnd.Next(0, 1000);
+                    num2 = rnd.Next(0, 1000);
+                }
+                int sum = num1 + num2;
+                string numb1 = num1.ToString();
+                string numb2 = num2.ToString();
+                int rndNumOper = rnd.Next(1, 6);
+                int rndOper = rnd.Next(1, 3);
+                int rndsumres = 0;
+                switch (rndOper)
+                {
+                    case 1:
+                        rndsumres = num1 + num2 + rndNumOper;
+                        break;
+                    case 2:
+                        rndsumres = num1 + num2 - rndNumOper;
+                        break;
+                }
+                Question.Text = numb1 + " + " + numb2 + "=";
+                int rndPos = rnd.Next(1, 3);
+                switch (rndPos)
+                {
+                    case 1:
+                        Button1.Content = sum.ToString();
+                        Button2.Content = rndsumres;
+                        break;
+                    case 2:
+                        Button1.Content = rndsumres;
+                        Button2.Content = sum.ToString();
+                        break;
+                }
+                string strpoints = points.ToString();
+                Level.Text = strpoints + "/10";
+               
             }
             else
             {
-                Button2.Content = rndsumres;
-            }            
-        } 
+                Question.Text = "You won";
+            }
+        }
+
         private void Button_Click(object sender, RoutedEventArgs e)
-        {           
+        {
+            string strsum = sum.ToString();
+            if (Button1.Content.ToString() == strsum)
+            {
+                points++;
+            }
 
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-
+            string strsum = sum.ToString();
+            if (Button2.Content.ToString() == strsum)
+            {
+                points++;
+            }
         }
-        
     }
 }
